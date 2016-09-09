f8fb391cc4947e01b26d4b7c1907a0c28840a3d0
Michael Atkisson <mpatkisson@gmail.com>
2016-09-09 11:16:32 -0400

    Adds a ResigQuery Append method

--- Resig/ResigQuery/Append.cs
+++ Resig/ResigQuery/Append.cs
@@ -15,7 +15,41 @@ namespace Resig
 
         public ResigQuery Append(string htmlString)
         {
+            foreach(var element in Elements)
+            {
+                IElement created = Context.CreateElement("template");
+                created.InnerHtml = htmlString;
+                if (created.HasChildNodes)
+                {
+                    element.Append(created.ChildNodes.ToArray());
+                }
+            }
             return this;
+        }
+
+        public ResigQuery Append(ResigQuery query)
+        {
+            foreach (var element in query)
+            {
+                Append(element.OuterHtml);
+            }
+            return this;
+        }
+
+        // Covers Element and Text
+        public ResigQuery Append(INode node)
+        {
+            foreach (var element in Elements)
+            {
+                element.Append(node);
+            }
+            return this;
+        }
+
+        public ResigQuery Append(Func<string> func)
+        {
+            string html = func();
+            return Append(html);
         }
 
     }
